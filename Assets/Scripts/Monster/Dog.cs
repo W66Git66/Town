ffe776adcd1869bae9a62a18d112ed38c393d551
wwx <2120405615@qq.com>
2024-05-06 15:21:45 +0800

    5/6first

--- Assets/Scripts/Monster/Dog.cs
+++ Assets/Scripts/Monster/Dog.cs
@@ -5,59 +5,129 @@ using Pathfinding;
 
 public class Dog : MonoBehaviour
 {
-    //[SerializeField]private TimeState _state;//显示当前怪物处于白天还是黑夜
     [SerializeField] private float _speed;//怪物的移动速度
 
+    private EnemyStates curState;
+
+    [Header("目标")]
     public Transform player;
+
+    [Header("移动追击")]
+    public float currentSpeed = 0;
+    public Vector3 direction { get; private set; }
+
+    public float chaseDistance = 3f;//追击距离
+    public float attackDistance = 0.8f;//攻击距离
 
     [Header("Pathfinding")]
     private Seeker _seeker;
-    private List<Vector3> _pathPoints;//路径点
+    [SerializeField]private List<Vector3> _pathPoints;//路径点
     private int _curIndex;//路径点的索引
-    [SerializeField]private float pathFindCooldown;//搜寻路径的冷却
+    [SerializeField]private float pathFindCooldown=0.5f;//搜寻路径的冷却
     private float pathTimer = 0;//计时器
 
-    private IState curState;
+    [Header("攻击")]
+    public float meleeAttackDamage;//近战攻击伤害
+    public bool isAttack = true;
+    [HideInInspector] public float distance;
+    public LayerMask playerLayer;//表示玩家图层
+    public float AttackCooldownDuration = 2f;//冷却时间
+
+    [HideInInspector] public SpriteRenderer sr;
+    [HideInInspector] public Rigidbody2D rb;
+    [HideInInspector] public Animator anim;
+    [HideInInspector] public Collider2D enemyCollider;
 
     Dictionary<EnemyStates, IState> states = new Dictionary<EnemyStates, IState>();
     private void Awake()
     {
         _seeker=GetComponent<Seeker>();
 
-        states.Add(EnemyStates.Idle, new EnemyIdleState());
-        states.Add(EnemyStates.Chase, new EnemyChaseState());
-        states.Add(EnemyStates.Attack, new EnemyAttackState());
-        states.Add(EnemyStates.Patrol, new EnemyPatrolState());
-        states.Add(EnemyStates.Death, new EnemyDeathState());
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        enemyCollider = GetComponent<Collider2D>();//碰撞器组件
     }
 
-    public void TransationState(EnemyStates stateType)
+    private void Start()
     {
-        if(curState!=null)
-        {
-            curState.OnExit();
-        }
-        curState = states[stateType];
-        curState.OnEnter();
+        TransState(EnemyStates.Idle);
     }
+
+    private void Update()
+    {
+        if(player==null)
+        {
+            return;
+        }
+        float distance = Vector2.Distance(player.position, transform.position);
+        AutoPath();
+        direction = _pathPoints[_curIndex] - _pathPoints[0];
+        StateMachine();
+    }
+
+    private void FixedUpdate()
+    {
+        if(curState==EnemyStates.Chase)
+        {
+            Move(direction);
+        }
+    }
+
+    private void StateMachine()
+    {
+        switch(curState)
+        {
+            case EnemyStates.Idle :
+                break;
+            case EnemyStates.Chase:
+                break;
+            case EnemyStates.Attack:
+                break;
+            case EnemyStates.Patrol:
+                break;
+            case EnemyStates.Death:
+                break;
+        }    
+    }
+        
+    public void GetPlayerTransform()
+    {
+        Collider2D[] chaseColliders = Physics2D.OverlapCircleAll(transform.position, chaseDistance, playerLayer);
+
+        if (chaseColliders.Length > 0)//玩家在追击范围内
+        {
+            player = chaseColliders[0].transform;//获取玩家的Transform
+            distance = Vector2.Distance(player.position, transform.position);
+        }
+        else
+        {
+            player = null;//玩家在追击范围外
+        }
+    }
+    #region 自动寻路
     private void AutoPath()
     {
         pathTimer += Time.deltaTime;
-        if(pathTimer > pathFindCooldown) 
+        if(pathTimer >= pathFindCooldown) 
         {
             GetPathPoints(player.position);
             pathTimer = 0;
         }
-        if (_pathPoints == null||_pathPoints.Count<0)
+        if (_pathPoints == null||_pathPoints.Count<0||_pathPoints.Count <= _curIndex)
         {
             GetPathPoints(player.position);
         }
-        else if (Vector3.Distance(transform.position, _pathPoints[_curIndex]) <= 0.1f)
+        else if(_curIndex<_pathPoints.Count)
         {
-            _curIndex++;
-            if( _curIndex >= _pathPoints.Count )
+            if (Vector2.Distance(transform.position, _pathPoints[_curIndex]) <= 0.1f)
             {
+
+                _curIndex++;
+                if( _curIndex >= _pathPoints.Count )
+                {
                 GetPathPoints(player.position);
+                }
             }
         }
     }
@@ -71,4 +141,34 @@ public class Dog : MonoBehaviour
         });
     }
 
+    #endregion
+
+    #region 移动
+    public void Move(Vector2 MovementInput)
+    {
+        if (MovementInput.magnitude > 0.1f && currentSpeed >= 0)
+        {            
+           
+            rb.velocity = MovementInput.normalized * currentSpeed;
+            //敌人左右翻转
+            if (MovementInput.x < 0)//左
+            {
+                sr.flipX = false;
+            }
+            if (MovementInput.x > 0)//右
+            {
+                sr.flipX = true;
+            }
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+    #endregion
+
+    private void TransState(EnemyStates states)
+    {
+        curState = states;
+    }
 }
