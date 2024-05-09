363584fde852627955d51b66765a5ba310d2a35b
Kuz <1424894032@qq.com>
2024-05-09 16:31:25 +0800

    新增 数据状态 测试

--- Assets/Scripts/Bag/DataSaveManager.cs
+++ Assets/Scripts/Bag/DataSaveManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
 [System.Serializable]
 public class PlayerBag
 {
@@ -10,9 +11,19 @@ public class PlayerBag
     public bool isKnifeOn = false;
 
 }
+*/
 public class DataSaveManager : MonoBehaviour
 {
-    public PlayerBag bag = new PlayerBag();
+    //public PlayerBag bag = new PlayerBag();
+
+    [Header("死去的麻雀数量")]
+    public int deadBird=0;
+
+    [Header("活蹦乱跳的麻雀数量")]
+    public int liveBird=0;
+
+    [Header("是否持有煞气刀")]
+    public bool isKnifeOn=false;
 
     void Start()
     {
@@ -22,5 +33,47 @@ public class DataSaveManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void GetDeadBird()//获得死去的麻雀
+    {
+        deadBird++;
+    }
+    public void UseDeadBird()//使用死去的麻雀
+    {
+        deadBird--;
+    }
+
+    public void GetLiveBird()//获得活蹦乱跳的麻雀
+    {
+        if (deadBird <= 0)
+        {
+            return;
+        }
+        else if (deadBird > 0)
+        {
+            liveBird = deadBird;
+        }
+    }
+    public void UseLiveBird()//使用活蹦乱跳的麻雀
+    {
+        if(liveBird <= 0)
+        {
+            return;
+        }
+        else if(liveBird > 0)
+        {
+            liveBird--;
+        }
+    }
+
+    public void GetKnife()//获得煞气刃
+    {
+        isKnifeOn = true;
+    }
+
+    public void NightReSet()//夜晚刷新
+    {
+        deadBird = liveBird = 0;
     }
 }
---
+++ Assets/Scripts/Bag/ObjectsAddTest.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//测试 增加死去的麻雀 按钮
+public class ObjectsAddTest : MonoBehaviour
+{
+    private Button addDeadBird;
+    private DataSaveManager dataSaveManager;
+    private void Start()
+    {
+        dataSaveManager = GameObject.Find("DataSaveManager").GetComponent<DataSaveManager>();
+        addDeadBird= gameObject.GetComponent<Button>();
+        addDeadBird.onClick.AddListener(AddDeadBird);
+    }
+
+    private void AddDeadBird()
+    {
+        dataSaveManager.GetDeadBird();
+        Debug.Log("+1");
+    }
+}
