56aa0a1bc1173bf1a269350436aada028bf113c7
wwx <2120405615@qq.com>
2024-06-21 12:49:52 +0800

    no message

--- Assets/Scripts/NPC/角色对话/新手引导/ScarecrowYinDao.cs
+++ Assets/Scripts/NPC/角色对话/新手引导/ScarecrowYinDao.cs
@@ -8,12 +8,8 @@ public class ScarecrowYinDao : MonoBehaviour
     {
         if (gameObject.GetComponent<NPC>().isOver == true)
         {
-<<<<<<< HEAD
             DataSaveManager.Instance.SetScarecrowYinDao();            
-=======
             DataSaveManager.Instance.isScareDes = true;
-            DataSaveManager.Instance.SetScarecrowYinDao();
->>>>>>> 8fe6210937c01035acf5a4173b9b486a162648f8
         }
         if (DataSaveManager.Instance.isScarecrowYinDao)
         {
