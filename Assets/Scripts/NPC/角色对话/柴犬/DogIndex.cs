51ac1c2416f7edaf6fb5d1ded31143942284932f
Kuz <1424894032@qq.com>
2024-06-21 11:36:58 +0800

    no message

--- Assets/Scripts/NPC/角色对话/柴犬/DogIndex.cs
+++ Assets/Scripts/NPC/角色对话/柴犬/DogIndex.cs
@@ -10,24 +10,9 @@ public class DogIndex : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
-        if(DataSaveManager.Instance.isJirouBack)
-        {
-            n = Random.Range(3, 6);//随机3-5个
-            m = Random.Range(3, 6);
-        }
-        else
-        {
-            n = Random.Range(1, 4);//随机1-3个
-            m = Random.Range(1, 4);
-        }
         if (DataSaveManager.Instance.isProteinEverbeenFound)
         {
             GameObject.Find("柴犬").GetComponent<DialogueIndex>().AddToIndex(2);
-            DataSaveManager.Instance.GetDeadBird(n);
-            if (DataSaveManager.Instance.isShenSheBack)
-            {
-                DataSaveManager.Instance.GetDeadBird(m);
-            }
             animator.SetTrigger("IsFull");
         }
         if (DataSaveManager.Instance.canJirouBack)
